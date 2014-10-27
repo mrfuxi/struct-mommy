fbf5f3e29afa4c75f87ec75726098d96b55a6b1c
Karol Dulęba <mr.fuxi@gmail.com>
2014-10-27 20:05:54 +0000

    Allow to define fields in sub structs

--- post_funcs.go
+++ post_funcs.go
@@ -3,8 +3,8 @@ package structmommy
 import (
     "errors"
     "fmt"
-    "log"
     "reflect"
+    "strings"
 )
 
 // Define is post Make function. Lets define values of struct fields
@@ -13,33 +13,88 @@ func Define(keyValue ...interface{}) func(interface{}) error {
 
     postFunc := func(obj interface{}) (err error) {
         reflectedVal := reflect.ValueOf(obj).Elem()
-        if reflectedVal.Kind() != reflect.Struct {
-            msg := fmt.Sprintf(
-                "Define on %s does not make sense. Only structs are accepted",
-                reflectedVal.Kind(),
-            )
-            return errors.New(msg)
-        }
-
-        for key, value := range defined {
-            f := reflectedVal.FieldByName(key)
-            newVal := reflect.ValueOf(value)
-
-            if newVal.Type() != f.Type() {
-                if newVal.Type().ConvertibleTo(f.Type()) {
-                    newVal = newVal.Convert(f.Type())
-                } else {
-                    log.Printf("Type problem: %v vs %v. Skip", newVal.Type(), f.Type())
-                    continue
-                }
-            }
-            f.Set(newVal)
-        }
-
-        return
+        return applyDefinedMap(reflectedVal, defined)
     }
 
     return postFunc
+}
+
+func applyDefinedMap(reflectedVal reflect.Value, defined map[string]interface{}) (err error) {
+    for key, value := range defined {
+        err = applyDefinedField(reflectedVal, key, value)
+        if err != nil {
+            continue
+        }
+    }
+
+    return
+}
+
+func applyDefinedField(reflectedVal reflect.Value, fieldName string, value interface{}) (err error) {
+    var field reflect.Value
+
+    //If field has dots, try to go though struct
+    if strings.Contains(fieldName, ".") {
+        fields := strings.SplitN(fieldName, ".", 2)
+        directField := fields[0]
+        furtherFields := fields[1]
+
+        if field, err = fieldByName(reflectedVal, directField); err != nil {
+            return
+        }
+
+        return applyDefinedField(field, furtherFields, value)
+    }
+
+    if err = hasFields(reflectedVal); err != nil {
+        return
+    }
+
+    if field, err = fieldByName(reflectedVal, fieldName); err != nil {
+        return
+    }
+
+    newVal := reflect.ValueOf(value)
+
+    if newVal.Type() != field.Type() {
+        if newVal.Type().ConvertibleTo(field.Type()) {
+            newVal = newVal.Convert(field.Type())
+        } else {
+            err = fmt.Errorf(
+                "could not convert between %v (src) and %v (dst)",
+                newVal.Type(),
+                field.Type(),
+            )
+            return
+        }
+    }
+    field.Set(newVal)
+
+    return
+}
+
+func fieldByName(reflectedVal reflect.Value, fieldName string) (field reflect.Value, err error) {
+    field = reflectedVal.FieldByName(fieldName)
+    if !field.IsValid() {
+        err = fmt.Errorf(
+            "could not find field '%s' in %s struct",
+            fieldName,
+            reflectedVal.Type(),
+        )
+        return
+    }
+    return
+}
+
+func hasFields(reflectedVal reflect.Value) (err error) {
+    if reflectedVal.Kind() != reflect.Struct {
+        err = fmt.Errorf(
+            "define on %s does not make sense; only structs are accepted",
+            reflectedVal.Kind(),
+        )
+        return
+    }
+    return
 }
 
 func variadicToMap(keyValue ...interface{}) map[string]interface{} {
--- post_funcs_test.go
+++ post_funcs_test.go
@@ -28,11 +28,95 @@ func TestPartiallyRandomTypeConversion(t *testing.T) {
     assert.NotEmpty(t, obj.FieldFloat)
 }
 
+func TestPartiallyRandomInvalidField(t *testing.T) {
+    obj := testMultipleSimpleFields{}
+
+    err := structmommy.Make(&obj, structmommy.Define("INVALID", 1))
+
+    assert.NotEmpty(t, obj.FieldInt, 2)
+    assert.NotEmpty(t, obj.FieldStr, "1")
+    assert.NotEmpty(t, obj.FieldFloat)
+    assert.EqualError(t, err, "could not find field 'INVALID' in structmommy_test.testMultipleSimpleFields struct")
+}
+
 func TestPartiallyRandomBasicString(t *testing.T) {
     var obj string
 
     err := structmommy.Make(&obj, structmommy.Define("Blah", 123))
 
-    assert.Equal(t, "Define on string does not make sense. Only structs are accepted", err.Error())
+    assert.EqualError(t, err, "define on string does not make sense; only structs are accepted")
     assert.NotEmpty(t, obj)
+}
+
+func TestPartiallyRandomEmbeddedNamedStruct(t *testing.T) {
+    obj := testNamedEmbeddedStructs{}
+
+    err := structmommy.Make(
+        &obj,
+        structmommy.Define("FInt.Field", 123),
+    )
+
+    assert.NoError(t, err)
+    assert.Equal(t, 123, obj.FInt.Field)
+}
+
+func TestPartiallyRandomEmbeddedNamedStructInvalidName(t *testing.T) {
+    obj := testNamedEmbeddedStructs{}
+
+    err := structmommy.Make(
+        &obj,
+        structmommy.Define("FInt.INVALID", 123),
+    )
+
+    assert.EqualError(t, err, "could not find field 'INVALID' in structmommy_test.testSingleInt struct")
+}
+
+func TestPartiallyRandomEmbeddedNamedStructInvalidType(t *testing.T) {
+    obj := testNamedEmbeddedStructs{}
+
+    err := structmommy.Make(
+        &obj,
+        structmommy.Define("FInt.Field.X", 123),
+    )
+
+    assert.EqualError(t, err, "define on int does not make sense; only structs are accepted")
+}
+
+func TestPartiallyRandomDeepEmbeddedNamedStruct(t *testing.T) {
+    obj := struct {
+        A struct {
+            B struct {
+                C struct {
+                    Value string
+                }
+            }
+        }
+    }{}
+
+    err := structmommy.Make(
+        &obj,
+        structmommy.Define("A.B.C.Value", "Bazinga!"),
+    )
+
+    assert.NoError(t, err)
+    assert.Equal(t, obj.A.B.C.Value, "Bazinga!")
+}
+
+func TestPartiallyRandomSetStruct(t *testing.T) {
+    obj := struct {
+        A struct {
+            B struct {
+                C testSingleString
+            }
+        }
+    }{}
+
+    newVal := testSingleString{Field: "Test"}
+    err := structmommy.Make(
+        &obj,
+        structmommy.Define("A.B.C", newVal),
+    )
+
+    assert.NoError(t, err)
+    assert.Equal(t, obj.A.B.C, newVal)
 }
