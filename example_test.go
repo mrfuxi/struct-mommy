5ae6f6de53b7cd0f22d1979ac9c76450e6c214e6
Karol Dulęba <mr.fuxi@gmail.com>
2014-10-27 08:22:01 +0000

    Add example for using Define

--- example_test.go
+++ example_test.go
@@ -22,3 +22,15 @@ func ExampleMake() {
     // FieldA -1.5426473e+38
     // FieldB 42
 }
+
+func ExampleDefine() {
+    obj := MyStruct{}
+
+    struct_mommy.SetSeed(26)
+    struct_mommy.Make(&obj, struct_mommy.Define("FieldA", 2.0))
+
+    fmt.Printf("FieldA %v\nFieldB %v", obj.FieldA, obj.FieldB)
+    // Output:
+    // FieldA 2
+    // FieldB 42
+}
